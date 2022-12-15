e312e2a2bdfa72c477b00a7953a88131e49fdeb7
fahim33-eng <77661612+fahim33-eng@users.noreply.github.com>
2022-12-14 21:51:51 -0800

    Add files via upload

---
+++ globalVariables.h
@@ -0,0 +1,31 @@
+#ifndef GLOBAL
+#define GLOBAL
+
+#include <bits/stdc++.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+//All Global Variables has been Declared here
+
+extern SDL_Window *window
+extern SDL_Renderer *renderer
+extern SDL_Event *event
+extern int is_running
+extern int is_paused
+extern Uint32 delay_event
+extern Uint32 mainmenu
+extern Uint32 startTime, currentTime
+extern int totalEnemy
+extern int score
+extern int frameNumber
+extern bool flag
+
+#define WINDOW_HEIGHT 800
+#define WINDOW_WIDTH 600
+#define WINDOW_WIDTH 600
+#define TITLE "Militia Combat"
+#define SCORE 0
+#define SPEED 5
+
+#endif
---
+++ header.h
@@ -0,0 +1,11 @@
+#ifndef HEADER
+
+#define HEADER
+
+#include "gameLogic.h"
+#include "mediaLoader.h"
+#include "initEverything.h"
+#include "closeEverything.h"
+#include "globalVariables.h"
+
+#endif
