a908c0e83c6b4a96917c7d384bbf067273205dad
fahim33-eng <77661612+fahim33-eng@users.noreply.github.com>
2022-12-14 21:52:36 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,26 +1,11 @@
 #include<bits/stdc++.h>
 #include<SDL2/SDL.h>
-
 using namespace std;
 
-const int WIDTH = 800 , HEIGHT = 600;
+#include "heaader.h";
+
 
 int main() {
-    SDL_Init(SDL_INIT_EVERYTHING);
-
-    SDL_Window *window = SDL_CreateWindow("Militia Combat", SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, HEIGHT, WIDTH, 1);
-    SDL_Event event;
-
-    while( 1 ) {
-        if (SDL_PollEvent( &event )) {
-            if (SDL_QUIT == event.type) {
-                break;
-            }
-        }
-    }
-    SDL_Delay(50000);
-    SDL_DestroyWindow( window );
-    SDL_Quit();
-
+    
 
 }
